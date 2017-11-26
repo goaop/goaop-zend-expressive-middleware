bde801332804ef34080ca3b26b22483cdaa83c20
Martin Reinfandt <reinfandt@z-em.de>
2017-11-26 14:01:51 +0100

    add test aspect

---
+++ tests/Aspect/TestAspect.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Reinfi\GoAop\ExpressiveMiddleware\Tests\Aspect;
+
+use Go\Aop\Aspect;
+
+/**
+ * @package Reinfi\GoAop\ExpressiveMiddleware\Tests\Aspect
+ */
+class TestAspect implements Aspect
+{
+
+}
